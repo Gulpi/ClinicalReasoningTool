dd798ee5b86029204bdd275560079ee152a0a365
ingahege <email@ingahege.de>
2017-08-24 16:14:43 +0200

    Polish language support

--- ClinReasonTool/src/java/application/AppBean.java
+++ ClinReasonTool/src/java/application/AppBean.java
@@ -37,7 +37,7 @@ import properties.IntlConfiguration;
 public class AppBean extends ApplicationWrapper implements HttpSessionListener{
 
 	public static final String DEFAULT_LOCALE="en"; //TODO get from properties!
-	public static final String[] ACCEPTED_LOCALES = new String[]{"en", "de"}; //TODO get from properties!
+	public static final String[] ACCEPTED_LOCALES = new String[]{"en", "de", "pl"}; //TODO get from properties!
 	public static List<Graph> graphs;
 	public static final String APP_KEY = "AppBean";
 	public static IntlConfiguration intlConf;

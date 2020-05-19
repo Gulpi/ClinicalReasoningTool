f21d0827d2c9ccfb103317f6f8e846f5ae28e942
Gulpi <bitbucket@gulpi.de>
2020-05-19 10:12:40 +0200

    Learning Analytics ws

--- ClinReasonTool/src/java/api/impl/LearningAnalytics1.java
+++ ClinReasonTool/src/java/api/impl/LearningAnalytics1.java
@@ -1,27 +1,37 @@
 package api.impl;
 
-import java.io.StringWriter;
-import java.util.Iterator;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
 
 import javax.faces.context.FacesContext;
+import javax.json.JsonArray;
 import javax.servlet.http.HttpServletRequest;
 
 import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import api.ApiInterface;
-import database.HibernateSession;
 import database.HibernateUtil;
+import net.casus.util.CasusConfiguration;
 import net.casus.util.StringUtilities;
 import net.casus.util.Utility;
+import net.casus.util.database.HQLQuery;
+import properties.IntlConfiguration;
 
 /**
  * Sample ApiInterface implemenation for demo
  * 
  * hqlQuery6 & 
-http://m7.instruct.de:8080/crt/src/html/api/api.xhtml?impl=la&in_case=1151758_2&in_ext_user=222112,208121,223276,200091,209297,222264,222369,222114,222116,225227,221513,210188,220797,237601,222181,224097,220937,246452,15739,237514,239659,221464,222205,247015,207402,226450,246352,225702,205557,208856,245105,239713,227639,240699,238449,244817,207979,210603,221674,210179,221353,182021,225339,210892,226187,222321,194601,226526,210660,232640,222224,194435,207496,223647,206186,207938,221599,226266,248619,208803,210575,210577,225513,224764,207747,210403,226461,223316,198792,226430,223352,223373,246143,223572,219310,236537,237454,202436,241491,241493,209244,160784,71500,177661,227408,234415,151796,234478,7875,156696,7900,200233,219986,232473,231054,231203,95846,231329) *
+http://m7.instruct.de:8080/crt/src/html/api/api.xhtml?impl=la&in_case=1151758_2&in_ext_user=222112,208121,223276,200091,209297,222264,222369,222114,222116,225227,221513,210188,220797,237601,222181,224097,220937,246452,15739,237514,239659,221464,222205,247015,207402,226450,246352,225702,205557,208856,245105,239713,227639,240699,238449,244817,207979,210603,221674,210179,221353,182021,225339,210892,226187,222321,194601,226526,210660,232640,222224,194435,207496,223647,206186,207938,221599,226266,248619,208803,210575,210577,225513,224764,207747,210403,226461,223316,198792,226430,223352,223373,246143,223572,219310,236537,237454,202436,241491,241493,209244,160784,71500,177661,227408,234415,151796,234478,7875,156696,7900,200233,219986,232473,231054,231203,95846,231329
+http://m7.instruct.de:8080/crt/src/html/api/api.xhtml?impl=la&in_case=909355_2&in_ext_user=7900,11138,
  * 
  * @author Gulpi (=Martin Adler)
  */
@@ -29,57 +39,113 @@ public class LearningAnalytics1 implements ApiInterface {
 
 	@Override
 	public String handle() {
- 		String result = "";
+		long startms = System.currentTimeMillis();
+		CasusConfiguration.addGlobalProperties("LearningAnalytics");
+		long endms = System.currentTimeMillis();
+		System.out.println("init "+(endms-startms) + "ms");
+		
+		String result = "";
+		Map resultObj = new HashMap();
+ 		List<Map> props = new ArrayList<Map>();
 		try {
 			String in_case = (String) ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest()).getParameter("in_case");
 			String in_ext_user = (String) ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest()).getParameter("in_ext_user");
-			
-			String hqlQuery6 = "select listItemId,count(*) from beans.relation.RelationDiagnosis where destId in ( " + 
-					"select id from beans.scripts.PatientIllnessScript where vp_id = :in_case and userId in (\n" + 
-					"select userId from beans.user.User where extUserId2 in ( :in_ext_user ) ) )\n" + 
-					"group by listItemId";
-			
-			String hqlQuery = "select sourceId,count,(select name from beans.list.ListItem li where li.item_id = sourceId) from ( " + 
-					"select listItemId as sourceId,count(*) as count from beans.relation.RelationDiagnosis where destId in ( " + 
-					"select id from beans.scripts.PatientIllnessScript where  userId in ( " + 
-					"select userId from beans.user.User where extUserId in ( :in_ext_user) " + 
-					"and vp_id = :in_case\n" + 
-					")\n" + 
-					")\n" + 
-					"group by listItemId;";
-			
-			String hql = hqlQuery6;
-			Session session = HibernateUtil.impl.getHibernateSession();
-			Query query = session.createQuery(hql);
-			if (hql.contains(":in_case")) {
-				query.setString("in_case", in_case);
+			String in_local = (String) ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest()).getParameter("in_locale");
+			Locale locale = Locale.ENGLISH;
+			if (StringUtilities.isValidString(in_local)) {
+				locale = new Locale(in_local);
 			}
+			
+			Map cell_query_parameter = new HashMap();
+			cell_query_parameter.put("in_case", in_case);
 			List<String> myList = StringUtilities.getStringListFromString(in_ext_user, ",");
-			if (hql.contains(":in_ext_user")) {
-				query.setParameterList("in_ext_user", myList);
-			}
+			cell_query_parameter.put("in_ext_user", myList);
 			
-			StringWriter sw = new StringWriter();
-			List l = query.list();
-			if (l != null) {
-				Iterator it = l.iterator();
-				while (it.hasNext()) {
-					Object[] loop = (Object[]) it.next();
-					for (int i=0;i<loop.length;i++) {
-						if (i>0) {
-							sw.append("\t");
-						}
-						sw.append(loop[i].toString());
-					}
-					sw.append("\n");
-				}
-			}
-			result = sw.toString();
-			
+			this.handleSection(cell_query_parameter, "LearningAnalytics1.hqlQuery1" , "LearningAnalytics1.hqlQuery2", props, "diagnosis", "LA.API.diagnosis", locale);
+			this.handleSection(cell_query_parameter, "LearningAnalytics1.hqlQuery3" , "LearningAnalytics1.hqlQuery4", props, "management", "LA.API.management", locale);
+			this.handleSection(cell_query_parameter, "LearningAnalytics1.hqlQuery5" , "LearningAnalytics1.hqlQuery6", props, "problems", "LA.API.problems", locale);
+			this.handleSection(cell_query_parameter, "LearningAnalytics1.hqlQuery7" , "LearningAnalytics1.hqlQuery8", props, "tests", "LA.API.tests", locale);
+			this.handleSummaryStatements(cell_query_parameter, props, "summaryStatements", "LA.API.summaryStatements", locale);
 		} catch (HibernateException e) {
-			result = Utility.stackTraceToString(e);
+			resultObj.put("result", "error");
+			resultObj.put("error", Utility.stackTraceToString(e));
+		}
+		
+		try {
+			resultObj.put("result", "ok");
+			ObjectMapper mapper = new ObjectMapper();
+			resultObj.put("data", props);
+			
+			startms = endms;
+			if (CasusConfiguration.getGlobalBooleanValue("LearningAnalytics1.prettyJSON", true)) {
+				result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(resultObj);
+			}
+			else {
+				result = mapper.writeValueAsString(resultObj);
+			}
+			endms = System.currentTimeMillis();
+		} catch (JsonProcessingException e) {
+			result = "{ result=\"error\", \"" + e.getMessage() + "\" }";
 		}
 		return result;
+	}
+
+	public void handleSummaryStatements(Map cell_query_parameter, List<Map> props, String key, String headlineKey, Locale locale) {
+		Map item = new HashMap();
+		props.add(item);
+		
+		item.put("key", key);
+		item.put("type", "textlist");
+		
+		String tmp_headline = IntlConfiguration.getValue(headlineKey, locale);
+		item.put("headline", tmp_headline!=null? tmp_headline : key);
+		
+		String tmp_desc = IntlConfiguration.getValue(headlineKey + ".desc", locale);
+		if (StringUtilities.isValidString(tmp_desc)) {
+			item.put("description", tmp_desc);
+		}
+		
+		HQLQuery hqlQuery = new HQLQuery("LearningAnalytics1.hqlQuerySummaryStatement", "", HibernateUtil.impl);
+		hqlQuery.setQuery_parameter(cell_query_parameter);
+		hqlQuery.query();		
+		
+		List<Properties> myresult = hqlQuery.resultsToPropertiesList(hqlQuery.getReturnAliases());
+		if (myresult != null) {
+			item.put("data", myresult);
+		}
+	}
+
+	public void handleSection(Map cell_query_parameter, String hqlKey1, String hqlKey2, List<Map> props, String key, String headlineKey, Locale locale) {
+		Map item = new HashMap();
+		props.add(item);
+		
+		item.put("key", key);
+		item.put("type", "wordcloud");
+		
+		String tmp_headline = IntlConfiguration.getValue(headlineKey, locale);
+		item.put("headline", tmp_headline!=null? tmp_headline : key);
+		
+		String tmp_desc = IntlConfiguration.getValue(headlineKey + ".desc", locale);
+		if (StringUtilities.isValidString(tmp_desc)) {
+			item.put("description", tmp_desc);
+		}
+		
+		HQLQuery hqlQuery = new HQLQuery(hqlKey1, "", HibernateUtil.impl);
+		hqlQuery.setQuery_parameter(cell_query_parameter);
+		hqlQuery.query();
+		String[] returnAliases = hqlQuery.getReturnAliases();
+		
+		hqlQuery.copyToQuery(hqlKey2, "", 
+				CasusConfiguration.getGlobalIntegerValue("LearningAnalytics1.listItemId1.Idx", 0), 
+				CasusConfiguration.getGlobalIntegerValue("LearningAnalytics1.listItemId1.Idx", 0), 
+				CasusConfiguration.getIntegerArrayValue("LearningAnalytics1.srcCopyCols", "1"), 
+				CasusConfiguration.getIntegerArrayValue("LearningAnalytics1.dstCopyCols", "2"));
+		
+		List<Properties> myresult = hqlQuery.resultsToPropertiesList(returnAliases);
+		if (myresult != null) {
+			item.put("data", myresult);
+		}
+		
 	}
 
 }

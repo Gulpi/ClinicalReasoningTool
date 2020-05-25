7a2bae018481cdb9753288ed357a928440edca4e
Gulpi <bitbucket@gulpi.de>
2020-05-25 17:59:28 +0200

    learning analytics

--- ClinReasonTool/src/java/api/impl/LearningAnalytics1.java
+++ ClinReasonTool/src/java/api/impl/LearningAnalytics1.java
@@ -27,7 +27,7 @@ import util.CRTLogger;
  * Sample ApiInterface implemenation for demo
  * 
  * hqlQuery6 & 
-http://m7.instruct.de:8080/crt/src/html/api/api.xhtml?impl=la&in_case=1151758_2&in_ext_user=222112,208121,223276,200091,209297,222264,222369,222114,222116,225227,221513,210188,220797,237601,222181,224097,220937,246452,15739,237514,239659,221464,222205,247015,207402,226450,246352,225702,205557,208856,245105,239713,227639,240699,238449,244817,207979,210603,221674,210179,221353,182021,225339,210892,226187,222321,194601,226526,210660,232640,222224,194435,207496,223647,206186,207938,221599,226266,248619,208803,210575,210577,225513,224764,207747,210403,226461,223316,198792,226430,223352,223373,246143,223572,219310,236537,237454,202436,241491,241493,209244,160784,71500,177661,227408,234415,151796,234478,7875,156696,7900,200233,219986,232473,231054,231203,95846,231329
+http://m7.instruct.de:8080/crt/src/html/api/api.xhtml?impl=la&in_case=1151758_2&problems=1&diagnosis=1&management=1&tests=1&correct=1&in_ext_user=222112,208121,223276,200091,209297,222264,222369,222114,222116,225227,221513,210188,220797,237601,222181,224097,220937,246452,15739,237514,239659,221464,222205,247015,207402,226450,246352,225702,205557,208856,245105,239713,227639,240699,238449,244817,207979,210603,221674,210179,221353,182021,225339,210892,226187,222321,194601,226526,210660,232640,222224,194435,207496,223647,206186,207938,221599,226266,248619,208803,210575,210577,225513,224764,207747,210403,226461,223316,198792,226430,223352,223373,246143,223572,219310,236537,237454,202436,241491,241493,209244,160784,71500,177661,227408,234415,151796,234478,7875,156696,7900,200233,219986,232473,231054,231203,95846,231329
 http://m7.instruct.de:8080/crt/src/html/api/api.xhtml?impl=la&in_case=909355_2&in_ext_user=7900,11138,
  * 
  * @author Gulpi (=Martin Adler)
@@ -41,6 +41,7 @@ public class LearningAnalytics1 implements ApiInterface {
 		long endms = System.currentTimeMillis();
 		System.out.println("init "+(endms-startms) + "ms");
 		
+		long query_startms = endms;
 		String result = "";
 		Map resultObj = new HashMap();
  		List<Map> props = new ArrayList<Map>();
@@ -64,10 +65,11 @@ public class LearningAnalytics1 implements ApiInterface {
 			int my_count = hqlQuery.getCount();
 			resultObj.put("count", Integer.valueOf(my_count));
 			
-			this.handleSection(cell_query_parameter, "LearningAnalytics1.hqlQuery1" , "LearningAnalytics1.hqlQuery2", props, "diagnosis", locale);
-			this.handleSection(cell_query_parameter, "LearningAnalytics1.hqlQuery3" , "LearningAnalytics1.hqlQuery4", props, "management", locale);
 			this.handleSection(cell_query_parameter, "LearningAnalytics1.hqlQuery5" , "LearningAnalytics1.hqlQuery6", props, "problems", locale);
+			this.handleSection(cell_query_parameter, "LearningAnalytics1.hqlQuery1" , "LearningAnalytics1.hqlQuery2", props, "diagnosis", locale);
 			this.handleSection(cell_query_parameter, "LearningAnalytics1.hqlQuery7" , "LearningAnalytics1.hqlQuery8", props, "tests", locale);
+			this.handleSection(cell_query_parameter, "LearningAnalytics1.hqlQuery3" , "LearningAnalytics1.hqlQuery4", props, "management", locale);
+			this.handleSection(cell_query_parameter, "LearningAnalytics1.hqlQuery9" , "LearningAnalytics1.hqlQuery10", props, "correct", locale);
 			this.handleSummaryStatements(cell_query_parameter, props, "summaryStatements", locale);
 		} catch (HibernateException e) {
 			resultObj.put("result", "error");
@@ -75,7 +77,10 @@ public class LearningAnalytics1 implements ApiInterface {
 		}
 		
 		try {
+			endms = System.currentTimeMillis();
+			
 			resultObj.put("result", "ok");
+			resultObj.put("query-time-ms", Long.toString(endms-query_startms));
 			ObjectMapper mapper = new ObjectMapper();
 			resultObj.put("data", props);
 			
@@ -87,6 +92,7 @@ public class LearningAnalytics1 implements ApiInterface {
 				result = mapper.writeValueAsString(resultObj);
 			}
 			endms = System.currentTimeMillis();
+			CRTLogger.out("LearningAnalytics1.handle " +  (endms-startms), CRTLogger.LEVEL_PROD);
 		} catch (JsonProcessingException e) {
 			result = "{ result=\"error\", \"" + e.getMessage() + "\" }";
 		}
--- ClinReasonTool/src/java/beans/search/SearchResult.java
+++ ClinReasonTool/src/java/beans/search/SearchResult.java
@@ -18,12 +18,47 @@ public class SearchResult implements Serializable {
 	private String name;
 	private long sourceId;
 	
+	/**
+	 * 1=problem, 2=diagonsis, 3=test,4=management
+	 */
+	private int itemType = -1;
+	/**
+	 * 0 for main item, 1 for synonyns'
+	 */
+	private int synonym = -1;
+	/**
+	 * expert(s) patient illness script id
+	 */
+	private long pisId = -1;
+	
+	/**
+	 * when a synonym this would be set
+	 */
+	private long synId = -1;
+	
+	/**
+	 * explicit set, so an explicit set synonym can be detected by selSynId==synId
+	 */
+	private long selSynId = -1;
+	
 	public String getVpId() {return vpId;}
 	public void setVpId(String vpId) {this.vpId = vpId;}
 	public String getName() {return name;}
 	public void setName(String name) {this.name = name;}
 	public long getSourceId() {return sourceId;}
 	public void setSourceId(long sourceId) {this.sourceId = sourceId;}
+	
+	public int getItemType() { return itemType; }
+	public void setItemType(int itemType) { this.itemType = itemType; }
+	public int getSynonym() { return synonym; }
+	public void setSynonym(int synonym) { this.synonym = synonym; }
+	public long getPisId() { return pisId; }
+	public void setPisId(long pisId) { this.pisId = pisId; }
+	
+	public long getSynId() { return synId; }
+	public void setSynId(long synId) { this.synId = synId; }
+	public long getSelSynId() { return selSynId; }
+	public void setSelSynId(long selSynId) { this.selSynId = selSynId; }
 	
 	/**
 	 * @return vpId without the system identifier (e.g. 12345 instead of 12345_2)
